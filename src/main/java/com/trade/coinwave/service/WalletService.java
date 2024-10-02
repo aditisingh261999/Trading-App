712569a9f30ea4e9123569d247e07a12eaa6e9fd
Tysum <vinamraraaz@gmail.com>
2024-10-03 02:37:06 +0530

    Created Wallet Entity and Wallet Service

---
+++ src/main/java/com/trade/coinwave/service/WalletService.java
@@ -0,0 +1,17 @@
+package com.trade.coinwave.service;
+
+import com.trade.coinwave.model.Order;
+import com.trade.coinwave.model.User;
+import com.trade.coinwave.model.Wallet;
+
+import java.math.BigDecimal;
+
+public interface WalletService {
+
+    Wallet getUserWallet(User user);
+    Wallet addBalanceToUserWallet(User user, BigDecimal amount);
+    Wallet findUserWalletById(Long id);
+    Wallet walletToWalletTransfer(User sender, Wallet receiver, BigDecimal amount);
+    Wallet payOrderPayment(Order order, User user);
+
+}
